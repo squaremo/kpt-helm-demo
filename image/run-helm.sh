6679b27527e29771417cb761c7746fa795c6dde9
Michael Bridgen <mikeb@squaremobius.net>
2020-05-15 18:40:30 +0100

    Make better filenames for generated files
    Helm will chuck everything into <chart>/template, with the original
    file names. This is OK but the subdirectories are a bit annoying.
    
    The script now pipes the template output through `kpt fn source` and
    `kpt fn sink` so the resources get "standard" filenames.

--- image/run-helm.sh
+++ image/run-helm.sh
@@ -4,7 +4,8 @@ DIR=$(mktemp -d '/tmp/helm.XXXXXXX')
 
 # Write the input from stdin out as files
 kpt --stack-trace fn sink "$DIR"
-# Generate the chart
-helm template --generate-name --output-dir="${DIR}" ./*.tgz >/dev/null
+# Generate the chart. Piping it through `kpt fn source` standardises
+# the file paths based on the names.
+helm template --generate-name ./*.tgz | kpt fn source | kpt fn sink "$DIR"
 # Write the resulting files back out on stdout
 kpt --stack-trace fn source "$DIR"
