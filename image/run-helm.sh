6ef6107fff35041308415849daefeb329e4dd36a
Michael Bridgen <mikeb@squaremobius.net>
2020-05-16 12:06:25 +0100

    Extract release name and namespace from input
    The ResourceList kind has a field for functionConfig, which can be
    used to supply configuration to the container image. This commit adds
    code to the run-helm-sh script to extract that config and get
    Helm-specific values from it.
    
    This will come into play if you do, for example,
    
       kpt fn run instance --image helm-flux -- releaseName=flux
    
    (the trailing arguments are turned into a ConfigMap and put in the
    functionConfig slot by `kpt fn run`)

--- image/run-helm.sh
+++ image/run-helm.sh
@@ -1,11 +1,24 @@
 set -e
 
 DIR=$(mktemp -d '/tmp/helm.XXXXXXX')
+OUTDIR="$DIR/out"
+mkdir -p $OUTDIR
 
-# Write the input from stdin out as files
-kpt --stack-trace fn sink "$DIR"
+# Write the input from stdin out as files, and as one file so it can
+# be examined for functionConfig.
+tee $DIR/input.yaml | kpt --stack-trace fn sink "$OUTDIR"
+
+CONFIG="$DIR/config.yaml"
+# Get functionConfig, if any, and extract some values from it.
+yq read $DIR/input.yaml 'functionConfig' > "$CONFIG"
+
+releasename=$(yq read "$CONFIG" 'data.releaseName')
+namespace=$(yq read "$CONFIG" 'data.namespace')
+
 # Generate the chart. Piping it through `kpt fn source` standardises
 # the file paths based on the names.
-helm template --generate-name ./*.tgz | kpt fn source | kpt fn sink "$DIR"
+helm template "${releasename}" ./*.tgz \
+     --namespace "${namespace}" \
+    | kpt fn source | kpt fn sink "$OUTDIR"
 # Write the resulting files back out on stdout
-kpt --stack-trace fn source "$DIR"
+kpt --stack-trace fn source "$OUTDIR"
